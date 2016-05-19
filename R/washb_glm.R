f283d1f6f6e1afb2add12f7b7f2bff644c05d35f
“amertens” <andrew.mertens3@gmail.com>
2016-05-18 21:25:36 -0700

    Add forcedW paramater to allow user to choose variables from the adjustment set W to force into the final glm model (bypassing screening)

--- R/washb_glm.R
+++ R/washb_glm.R
@@ -8,6 +8,7 @@
 #' @param tr Binary treatment group variable (ideally a factor), comparison group first
 #' @param pair Pair-matched randomization ID variable (in WASH Benefits: block)
 #' @param W Optional data frame that includes adjustment covariates (for adjusted estimates)
+#' @param forcedW Optional vector of variable names to force as adjustment covariates (no screening)
 #' @param id ID variable for independent units (cluster ID)
 #' @param contrast Vector of length 2 that includes the groups to contrast, e.g., c("Control","Water")
 #' @param family GLM model family (gaussian, binomial, poisson). Negative binomial will be added in the near future.
@@ -16,9 +17,10 @@
 #' @export
 #'
 #' @examples
-washb_glm <- function(Y,tr,pair,W=NULL,id,contrast,family=gaussian) {
+washb_glm <- function(Y,tr,pair,W=NULL, forcedW=NULL, id,contrast,family=gaussian) {
   # Y     : outcome variable (continuous, such as LAZ, or binary, such as diarrhea)
   # tr    : binary treatment group variable, comparison group first
+  # pair  : Pair-matched randomization ID variable (in WASH Benefits: block)
   # W     : (optional) data frame that includes adjustment covariates
   # id    : id variable for independent units (e.g., cluster)
   # contrast : vector of length 2 that includes the tr groups to contrast
@@ -51,12 +53,31 @@ washb_glm <- function(Y,tr,pair,W=NULL,id,contrast,family=gaussian) {
   n.sub  <- dim(glmdat)[1]
   if(n.orig>n.sub) cat("\n-----------------------------------------\nDropping",n.orig-n.sub,"observations due to missing values in 1 or more variables\n","Final sample size:",n.sub,"\n-----------------------------------------\n")
 
+  #split W into screened and forced adjustment covariates
   if(!is.null(W)){
+    if(!is.null(forcedW)){
+      screenW<-subset(glmdat, select=colnames(W))
+      toexclude <- names(screenW) %in% forcedW
+      screenW=screenW[!toexclude]
+    }else{
+      screenW<-subset(glmdat, select=colnames(W))
+    }
+  }else{
+    screenW<-NULL
+  }
+  #forceWdata<-subset(glmdat, select=forcedW)
+
+  if(!is.null(screenW)){
     # pre-screen the covariates
     # see Wprescreen() in the base functions
     cat("\n-----------------------------------------\nPre-screening the adjustment covariates:\n-----------------------------------------\n")
-    Wscreen <- Wprescreen(Y=glmdat$Y,Ws=glmdat[,5:ncol(glmdat)],family=family)
-    dmat <- subset(glmdat,select=c("Y","tr",Wscreen,"pair"))
+    Wscreen <- Wprescreen(Y=glmdat$Y,Ws=screenW,family=family)
+
+    if(!is.null(forcedW)){
+      dmat <- subset(glmdat,select=c("Y","tr",Wscreen,forcedW,"pair"))
+    } else {
+      dmat <- subset(glmdat,select=c("Y","tr",Wscreen,"pair"))
+    }
   } else {
     dmat <- subset(glmdat,select=c("Y","tr","pair"))
   }
