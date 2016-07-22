0ba0feb27b788efeb64fc75100fb58a92e295af3
“amertens” <andrew.mertens3@gmail.com>
2016-07-21 18:30:03 -0700

    Startup message

---
+++ R/startup.R
@@ -0,0 +1,25 @@
+#'washb Package loading startup function
+#'
+#'
+#' @export
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to the washb package\nUse the command browsevignettes(\"washb\") to open the package vignette\nand learn how to use all the package functions.")
+}
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/washb",
+    devtools.install.args = "",
+    devtools.name = "Ben Arnold",
+    devtools.desc.author = '"Ben Arnold <benarnold@berkeley.edu> [aut, cre]"',
+    devtools.desc.license = "Creative Commons Attribution 3.0 Unported License",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+
+  invisible()
+}
