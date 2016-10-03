ad5d0edd44d5b54e3dc9d55a9e352349da57bad9
ben-arnold <benarnold@berkeley.edu>
2016-10-02 22:07:40 -0700

    Updated washb_tmle function. Removed 'modelfit' argument (unnecessary). Added Delta argument for IPCW estimation. Added documentation

--- R/washb_tmle.R
+++ R/washb_tmle.R
@@ -1,52 +1,63 @@
-#' Target maximum likelihood estimation function for the WASH Benefits study
+#' Target maximum likelihood estimation of intention-to-treat effects in the WASH Benefits trials
 #'
-#' Estimate intention-to-treat parameters using targeted maximum likelihood estimation (TMLE)
+#' Estimate intention-to-treat parameters using targeted maximum likelihood estimation (TMLE), potentially adjusted for covariates and missing outcomes
 #'
 #' @details
-#' The \code{washb_tmle} function is a convenience wrapper for the \code{\link[tmle]{tmle}} function. It estimates intention-to-treat effects in a trial using targeted maximum likelihood estimation. If the trial is pair-matched, be sure to specify the pair id in the \code{id} argument. If adjustment covariates (\code{W}) are specified, then by default they are pre-screened and the subset that is associated with the outcome based on a likelihood ratio test are used in the estimation. There are some other important defaults to be aware of. First, if the \code{prtr=NULL} (the default), then the function estimates the treatment mechanism rather than treating it as known (recommended). Second, you can specify \code{modelfit="sl"} to use the \code{\link[SuperLearner]{SuperLearner}} algorithm to fit the outcome and treatment models in the TMLE (the default).  Alternatively, you can specify \code{modelfit="glm"} to fit the outcome and treatment models using main effects regression (either linear or binomial, depending on the family). If you use super learning to fit models, then you can also specify the algorithm libraries using the \code{Q.SL.library} and \code{g.SL.library} arguments (pre-specified defaults are provided).
+#' The \code{washb_tmle} function is mainly a convenience wrapper for the \code{\link[tmle]{tmle}} function. It estimates intention-to-treat effects in a trial using targeted maximum likelihood estimation (TMLE). In brief, the function does the following: it restricts the data to complete observations in the two arms listed in the \code{contrast} argument, it pre-screens covariates (\code{W}), if specified, to select those that have a univariate association with the outcome, and then it estimates the intention-to-treat effect using TMLE.  If \code{family='binomial'}, then the function returns effects on the absolute, relative, and odds ratio scale. If \code{Delta} is specified (i.e., observations with missing outcomes are included), then the function will adjust the effects for missingness using inverse probability of censoring weights, with the weights estimated using super learning of \code{Pr(Delta|A,W)}.
+#'
+#' If the analysis is pair-matched (as for primary outcomes), be sure to specify the pair ID in the \code{id} argument. Do not include pair IDs in the adjustment covariate set.
+#'
+#' If adjustment covariates (\code{W}) are specified, then by default they are pre-screened and the subset that is associated with the outcome based on a likelihood ratio test are used in the estimation. There are some other important defaults to be aware of. First, if the argument \code{prtr=NULL} (the default), then the function estimates the treatment mechanism rather than treating it as known. Estimating the treatment mechanism theoretically increases precision and empirically we have found this as well (in the Bangladesh primary analysis). Second, the function uses the \code{\link[SuperLearner]{SuperLearner}} algorithm to adjust for covariates. The default algorithm library includes the simple mean, GLM, Bayes GLM with non-informative priors, generalized additive models (degree 2), and lasso (glmnet). You can type \code{listWrappers()} to see the full set of algorithms implemented in the super learner. If you just wish to use a main effects GLM model to adjust for the covariates, then you can specify \code{Q.SL.library="SL.glm"}.
+#'
+#' If you want to adjust for missing outcomes in the analysis, then you need to include observations that have a missing outcome (\code{Y}) with \code{Delta=0} for those observations. Observations with missing outcomes should have treatment (\code{tr}) and covariate (\code{W}) information, which are used to create weights for \code{Pr(Delta|A,W)}.
 #'
 #' Note: this function depends on the \code{\link[tmle]{tmle}} package, the \code{\link[SuperLearner]{SuperLearner}} package, as well as the internal washb_prescreen() and design_matrix() functions.
 #'
-#' Note: In the near future this function will allow for outcome censoring (missing data) using the \code{delta} argument.
 #'
 #' @param Y Outcome variable (continuous, such as LAZ, or binary, such as diarrhea)
 #' @param tr Binary treatment group variable, comparison group first
 #' @param W Data frame that includes adjustment covariates
 #' @param id ID variable for independent units. For pair-matched designs, this is the matched pair.
+#' @param Delta indicator of missing outcome. 1 - observed, 0 - missing.
 #' @param family Outcome family: \code{gaussian} (continuous outcomes, like LAZ) or \code{binomial} (binary outcomes like diarrhea or stunting)
-#' @param contrast Vector of length 2 that includes the tr groups to contrast
-#' @param prtr Vector of length 2 that includes the probability of receiving each treatment in the contrast argument for the main trial, the control is double sized (allocation 2:1) so control vs. active intervention should be \code{prtr=c(0.67,0.33)}. in contrasts where the arms are equally sized (allocation 1:1), then \code{prtr=c(0.5,0.5)}. If \code{prtr=NULL} (the default), then the treatment mechanism Pr(A|W) is estimated, which is usually more efficient (preferred).
-#' @param modelfit String variable of value \code{"sl"} for SuperLearner prediction or \code{"glm"} for GLM prediction of E(Y|A,W) and Pr(A|W).
+#' @param contrast Vector of length 2 that includes the treatment groups to contrast (e.g., \code{contrast=c('Control','Nutrition')}).
+#' @param prtr Vector of length 2 that includes the probability of receiving each treatment in the contrast argument for the main trial, the control is double sized (allocation 2:1) so control vs. active intervention should be \code{prtr=c(0.67,0.33)}. in contrasts where the arms are equally sized (allocation 1:1), then \code{prtr=c(0.5,0.5)}. If \code{prtr=NULL} (the default), then the treatment mechanism Pr(A|W) is estimated, which is usually more efficient (higher precision of the estimates).
 #' @param Q.SL.Library Library of algorithms to include in the SuperLearner for the outcome model
-#' @param g.SL.library Library of algorithms to include in the SuperLearner for the treatment model (ignored if prtr is specified)
-#' @param seed A seed for the pseudo-random cross-validation split (use for perfectly reproducible results if \code{modelfit="sl"}).
+#' @param g.SL.library Library of algorithms to include in the SuperLearner for the treatment model Pr(A|W) (ignored if prtr is specified), and for the missingness model Pr(Delta|A,W) (if Delta is specified)
+#' @param pval The p-value threshold used to pre-screen covariates (\code{W}) based on a likelihood ratio test in a univariate regression with the outcome (\code{Y}). Variables with a univariate association p-value below this threshold will be used in the final model. Defaults to 0.2.
+#' @param seed A seed for the pseudo-random cross-validation split used in model selection (use for perfectly reproducible results).
 #'
+#' @return A \code{tmle()} fit object (see the \code{\link[tmle]{tmle}} package for details). The \code{$estimates} list includes parameter estimates along with variance estimates and confidence intervals.
 #'
-#' @return A \code{tmle()} fit object (see the \code{\link[tmle]{tmle}} package for details).
+#' @references
+#' Gruber S, van der Laan M. tmle: An R Package for Targeted Maximum Likelihood Estimation. J Stat Softw. 2012;51: 1–35. \link{https://www.jstatsoft.org/article/view/v051i13}
 #'
-#' @export
+#' Balzer LB, van der Laan MJ, Petersen ML, SEARCH Collaboration. Adaptive pre-specification in randomized trials with and without pair-matching. Stat Med. 2016; doi:10.1002/sim.7023 \link{http://onlinelibrary.wiley.com/doi/10.1002/sim.7023/abstract}
 #'
 #' @examples
 #' # TBD
 #'
+#'@export
 
 
-washb_tmle <- function(Y,tr,W=NULL,id,family="gaussian",contrast,prtr=NULL,modelfit="sl",Q.SL.library=c("SL.mean","SL.glm","SL.bayesglm","SL.gam","SL.glmnet"),g.SL.library=Q.SL.library, seed=NULL) {
+washb_tmle <- function(Y,tr,W=NULL,id, Delta = rep(1,length(Y)),family="gaussian",contrast,prtr=NULL,Q.SL.library=c("SL.mean","SL.glm","SL.bayesglm","SL.gam","SL.glmnet"),g.SL.library=Q.SL.library, pval=0.2, seed=NULL) {
 
   require(tmle)
   require(SuperLearner)
 
-  # Make a data.frame that includes the relevant covariates
+  # Make a data.frame, restricted to the 2 arms in the contrast
   if(is.null(W)){
     tmledat <- data.frame(
       id=id[tr==contrast[1]|tr==contrast[2]],
       Y=Y[tr==contrast[1]|tr==contrast[2]],
+      Delta=Delta[tr==contrast[1]|tr==contrast[2]],
       tr=tr[tr==contrast[1]|tr==contrast[2]]
     )
   } else{
     tmledat <- data.frame(
       id=id[tr==contrast[1]|tr==contrast[2]],
       Y=Y[tr==contrast[1]|tr==contrast[2]],
+      Delta=Delta[tr==contrast[1]|tr==contrast[2]],
       tr=tr[tr==contrast[1]|tr==contrast[2]],
       W[tr==contrast[1]|tr==contrast[2],]
     )
@@ -63,63 +74,57 @@ washb_tmle <- function(Y,tr,W=NULL,id,family="gaussian",contrast,prtr=NULL,model
   # if specified, create a vector of treatment probabilities, depending on the type of contrast
   # for control vs. intervention, the control prob should be 2/3 and tr prob should be 1/3 (allocation 2:1)
   # for intervention arm 1 vs intervantion arm 2, prob should be 1/2 for both (allocation 1:1)
-  if(!is.null(prtr)) {
+  if(is.null(prtr)) {
+    tr_p <- NULL
+  } else {
     tr_p <- ifelse(tmledat$tr==contrast[1],prtr[1],prtr[2])
     g.SL.library <- NULL
-  } else {
-    tr_p <- NULL
   }
 
   # pre-screen the covariates (if specified)
   # see washb_prescreen() in the base functions
-  if(!is.null(W)){
-
-    cat("\n-----------------------------------------\nPre-screening the adjustment covariates:\n-----------------------------------------\n")
-    Wscreen <- washb_prescreen(Y=tmledat$Y,Ws=tmledat[,4:ncol(tmledat)],family=family,pval=0.2,print=TRUE)
-    Wselect <- subset(tmledat[,4:ncol(tmledat)],select=Wscreen)
-    cat("\n-----------------------------------------\n")
-
-    # for covariates that are factors, create indicator variables
-    # because the tmle() function cannot handle factors in W
-    # this relies on an internal function called design_matrix included in the washb package
-    Wselect <- design_matrix(Wselect)
-
-  } else{
+  if(is.null(W)){
     # if W is null, create two empty variables so that tmle() can run
     # (does not affect estimation)
     Wselect <- data.frame(w1=rep(1,length(tmledat$Y)),w2=rep(1,length(tmledat$Y)))
     Q.SL.library <- c("SL.glm")
     g.SL.library <- c("SL.glm")
+  } else{
+    cat("\n-----------------------------------------\nPre-screening the adjustment covariates:\n-----------------------------------------\n")
+    Wscreen <- washb_prescreen(Y=tmledat$Y,Ws=tmledat[,4:ncol(tmledat)],family=family,pval=pval,print=TRUE)
+
+    cat("\n-----------------------------------------\n")
+
+    # for covariates that are factors, create indicator variables
+    # because the tmle() function cannot handle factors in W
+    # this relies on an internal function called design_matrix included in the washb package
+    if(length(Wscreen)>0){
+      Wselect <- subset(tmledat[,4:ncol(tmledat)],select=Wscreen)
+      Wselect <- design_matrix(Wselect)
+    } else{
+      cat("\n\nNo covariates were associated with the outcome\nProceeding with no adjustment...")
+      # create a data frame with 2 columns of 1s -- allows tmle() to run, but does no adjustment
+      Wselect <- data.frame(w1=rep(1,length(tmledat$Y)),w2=rep(1,length(tmledat$Y)))
+    }
+
   }
 
   # re-parse the variables to use them in tmle()
   tmle_Y <- tmledat$Y
   tmle_A <- ifelse(tmledat$tr==contrast[2],1,0)
+  tmle_Delta <- tmledat$Delta
   tmle_id <- tmledat$id
-
-  # If the E(Y|A,W) and Pr(A|W) models are fit with GLM, make formula objects
-  if(modelfit=="sl") {
-    Qform <- NULL
-    gform <- NULL
-  }
-  if(modelfit=="glm") {
-    Qform <- as.formula(paste("Y~A+",paste(names(Wselect),collapse=" + ")))
-    gform <- as.formula(paste("A~",paste(names(Wselect),collapse=" + ")))
-    Q.SL.library <- NULL
-    g.SL.library <- NULL
-  }
 
   # estimate the ITT using TMLE
   if(!is.null(seed)) set.seed(seed)
   tmle_fit <- tmle(Y=tmle_Y,
                    A=tmle_A,
                    W=Wselect,
+                   Delta=tmle_Delta,
                    id=tmle_id,
                    family=family,
-                   Qform=Qform,
                    Q.SL.library=Q.SL.library,
                    g1W=tr_p,
-                   gform=gform,
                    g.SL.library=g.SL.library
   )
   cat("\n-----------------------------------------\nEstimation Results:\n-----------------------------------------\n")
