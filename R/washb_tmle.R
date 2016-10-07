47ac8172fd61353b925e254cdce10fa666c4487c
ben-arnold <benarnold@berkeley.edu>
2016-10-07 14:00:03 -0700

    Many refinements to washb_tmle for error handling, stability, printing. Removed option to specify treatment probabilities

--- R/washb_tmle.R
+++ R/washb_tmle.R
@@ -7,7 +7,11 @@
 #'
 #' If the analysis is pair-matched (as for primary outcomes), be sure to specify the pair ID in the \code{id} argument. Do not include pair IDs in the adjustment covariate set.
 #'
-#' If adjustment covariates (\code{W}) are specified, then by default they are pre-screened and the subset that is associated with the outcome based on a likelihood ratio test are used in the estimation. There are some other important defaults to be aware of. First, if the argument \code{prtr=NULL} (the default), then the function estimates the treatment mechanism rather than treating it as known. Estimating the treatment mechanism theoretically increases precision and empirically we have found this as well (in the Bangladesh primary analysis). Second, the function uses the \code{\link[SuperLearner]{SuperLearner}} algorithm to adjust for covariates. The default algorithm library includes the simple mean, GLM, Bayes GLM with non-informative priors, generalized additive models (degree 2), and lasso (glmnet). You can type \code{listWrappers()} to see the full set of algorithms implemented in the super learner. If you just wish to use a main effects GLM model to adjust for the covariates, then you can specify \code{Q.SL.library="SL.glm"}.
+#' If adjustment covariates (\code{W}) are specified, then by default they are pre-screened and the subset that is associated with the outcome based on a likelihood ratio test are used in the estimation. There are some other important defaults to be aware of. First, the \code{washb_tmle} function estimates the treatment mechanism even though it is a randomized trial. There are two reasons for this -- one theoretical and one practical. The theoretical reason is that estimating the treatment mechanism gains efficiency (see Balzer et al. 2016); the practical reason is that unless the analysis is conducted at the cluster level (i.e., providing cluster means to the \code{washb_tmle} function), then the empirical treatment probabilities differ slightly due to varying cluster sizes. Estimating the treatment mechanism ensures that the variance calculation correctly accounts for the empirical treatment probabilities in the data.
+#'
+#'Another default is that \code{washb_tmle} uses the \code{\link[SuperLearner]{SuperLearner}} algorithm to adjust for covariates and to predict the treatment mechanism and censoring mechanism (if adjusting for missing outcomes). The default algorithm library includes the simple mean, main terms GLM, main terms Bayes GLM with non-informative priors, generalized additive models (degree 2), and lasso (glmnet). These are the pre-specified algorithms from the original trial statistical analysis plan. You can type \code{listWrappers()} to see the full set of algorithms implemented in the super learner. If you just wish to use a main effects GLM model to adjust for the covariates, then you can specify \code{Q.SL.library="SL.glm"}.  If you are dealing with very small sample sizes (e.g., in a substudy), then you may wish to use even simpler libraries, such as a set of univariate regressions (as in Balzer et al. 2016).
+#'
+#'Finally, by default the function uses the same algorithm library to predict the outcome (\code{Q.SL.library}) and the treatment and censoring mechanisms (\code{g.SL.library}). You can specify a different library for the treatment and censoring mechanisms using the \code{g.SL.library} argument.
 #'
 #' If you want to adjust for missing outcomes in the analysis, then you need to include observations that have a missing outcome (\code{Y}) with \code{Delta=0} for those observations. Observations with missing outcomes should have treatment (\code{tr}) and covariate (\code{W}) information, which are used to create weights for \code{Pr(Delta|A,W)}.
 #'
@@ -17,12 +21,11 @@
 #' @param Y Outcome variable (continuous, such as LAZ, or binary, such as diarrhea)
 #' @param tr Binary treatment group variable, comparison group first
 #' @param W Data frame that includes adjustment covariates
-#' @param id ID variable for independent units. For pair-matched designs, this is the matched pair.
-#' @param pair (Optional if there is no missingness in pair-matching) The matched pair unit (In WASH Benefits, blocks). This argument it used to drop pair levels if there is missingness in one ore more levels of the contrast. There treatment missingness in the Kenya blocks (not all blocks contain all interventions) but not in the Bangladesh blocks,
+#' @param id ID variable for independent units. For pair-matched designs, this is the matched pair and should be the same as the \code{pair} argument. For analyses that are not pair-matched, then it should typically be the cluster.
+#' @param pair An optional ID variable to identify the matched pair unit (In WASH Benefits, blocks) if conducting a matched-pair analysis. This argument is used to drop pairs that are missing one or more treatment groups. Incomplete pairs is not an issue in the overall Bangladesh trial (there were no incomplete blocks), but is an issue in the Kenya trial where there were some incomplete blocks.
 #' @param Delta indicator of missing outcome. 1 - observed, 0 - missing.
 #' @param family Outcome family: \code{gaussian} (continuous outcomes, like LAZ) or \code{binomial} (binary outcomes like diarrhea or stunting)
 #' @param contrast Vector of length 2 that includes the treatment groups to contrast (e.g., \code{contrast=c('Control','Nutrition')}).
-#' @param prtr Vector of length 2 that includes the probability of receiving each treatment in the contrast argument for the main trial, the control is double sized (allocation 2:1) so control vs. active intervention should be \code{prtr=c(0.67,0.33)}. in contrasts where the arms are equally sized (allocation 1:1), then \code{prtr=c(0.5,0.5)}. If \code{prtr=NULL} (the default), then the treatment mechanism Pr(A|W) is estimated, which is usually more efficient (higher precision of the estimates).
 #' @param Q.SL.Library Library of algorithms to include in the SuperLearner for the outcome model
 #' @param g.SL.library Library of algorithms to include in the SuperLearner for the treatment model Pr(A|W) (ignored if prtr is specified), and for the missingness model Pr(Delta|A,W) (if Delta is specified)
 #' @param pval The p-value threshold used to pre-screen covariates (\code{W}) based on a likelihood ratio test in a univariate regression with the outcome (\code{Y}). Variables with a univariate association p-value below this threshold will be used in the final model. Defaults to 0.2.
@@ -45,77 +48,64 @@
 #' @examples
 
 
-washb_tmle <- function(Y,tr,W=NULL,id,pair=NULL, Delta = rep(1,length(Y)),family="gaussian",contrast,prtr=NULL,Q.SL.library=c("SL.mean","SL.glm","SL.bayesglm","SL.gam","SL.glmnet"),g.SL.library=Q.SL.library, pval=0.2, seed=NULL, print=TRUE) {
+washb_tmle <- function(Y,tr,W=NULL,id = 1:length(Y), pair=NULL, Delta = rep(1,length(Y)), family="gaussian",contrast,Q.SL.library=c("SL.mean","SL.glm","SL.bayesglm","SL.gam","SL.glmnet"),g.SL.library=Q.SL.library, pval=0.2, seed=NULL, print=TRUE) {
 
   require(tmle)
   require(SuperLearner)
-  #Create empty variable used in subgroup analysis
-  Subgroups=NULL
+
+
+  # for pair matched analyses, ensure that the pair-and the ID variables are the same
+  # throw an error and message if they are not the same
+  if(!is.null(pair)){
+    cat("\n-----------------------------------------","\nBy specifying the pair argument,\nyou have indicated that this is a matched pair analysis.\n\nNote: the analysis will only include pairs\nthat have a contrast in the treatment variable.","\n-----------------------------------------")
+
+    fnargs <- as.list( match.call() )
+    if(length(fnargs$id)==0) fnargs$id <- "(unspecified), which defaults to 1:length(Y)"
+
+    if(fnargs$pair[[length(fnargs$pair)]] != fnargs$id[[length(fnargs$id)]] ) {
+      stop(paste("\nBy specifying the pair argument, you have indicated this is a pair-matched analysis.\n\nTo get correct variance, the variable you pass to id\nmust be the same as the variable you pass to pair. \n\nIf you are not doing a pair-matched analysis, then specify pair=NULL\n(or leave it unspecified).\n\n   You specified pair=",fnargs$pair[[length(fnargs$pair)]],"and id=",fnargs$id[[length(fnargs$id)]]))
+    }
+
+  }
+
 
   # Make a data.frame, restricted to the 2 arms in the contrast
   if(is.null(W)){
     if(is.null(pair)){
-    tmledat <- data.frame(
-      id=id[tr==contrast[1]|tr==contrast[2]],
-      Y=Y[tr==contrast[1]|tr==contrast[2]],
-      Delta=Delta[tr==contrast[1]|tr==contrast[2]],
-      tr=tr[tr==contrast[1]|tr==contrast[2]]
-    )
+      tmledat <- data.frame(id,Y,Delta,tr)
     } else{
-      tmledat <- data.frame(
-        id=id[tr==contrast[1]|tr==contrast[2]],
-        Y=Y[tr==contrast[1]|tr==contrast[2]],
-        Delta=Delta[tr==contrast[1]|tr==contrast[2]],
-        tr=tr[tr==contrast[1]|tr==contrast[2]],
-        pair=pair[tr==contrast[1]|tr==contrast[2]]
-      )
+      tmledat <- data.frame(id,pair,Y,Delta,tr)
     }
   } else{
     if(is.null(pair)){
-    tmledat <- data.frame(
-      id=id[tr==contrast[1]|tr==contrast[2]],
-      Y=Y[tr==contrast[1]|tr==contrast[2]],
-      Delta=Delta[tr==contrast[1]|tr==contrast[2]],
-      tr=tr[tr==contrast[1]|tr==contrast[2]],
-      W[tr==contrast[1]|tr==contrast[2],]
-    )
+      tmledat <- data.frame(id,Y,Delta,tr,W)
     } else{
-      tmledat <- data.frame(
-        id=id[tr==contrast[1]|tr==contrast[2]],
-        Y=Y[tr==contrast[1]|tr==contrast[2]],
-        Delta=Delta[tr==contrast[1]|tr==contrast[2]],
-        tr=tr[tr==contrast[1]|tr==contrast[2]],
-        W[tr==contrast[1]|tr==contrast[2],],
-        pair=pair[tr==contrast[1]|tr==contrast[2]]
-      )
+      tmledat <- data.frame(id,pair,Y,Delta,tr,W)
     }
   }
-
+  tmledat <- subset(tmledat,tr==contrast[1]|tr==contrast[2])
   tmledat$tr <- factor(tmledat$tr,levels=contrast[1:2])
 
-
-
-
-  #####
-  #Block Dropping
-  #####
+  # if pair is specified, drop any pairs that have no treatment contrast in them
   if(!is.null(pair)){
-    #Drop blocks missing comparing arm 1
+    #Drop blocks missing contrast arm 1
     n.orig <- dim(tmledat)[1]
     miss<-NULL
     activeOnly<-((subset(tmledat,tr==contrast[1])))
     nomiss<-sort(unique(activeOnly$pair))
     miss1<-(unique(pair)[which(!( unique(pair)%in%(nomiss) ))])
 
-    #Drop blocks missing comparing arm 2
+    #Drop blocks missing contrast arm 2
     activeOnly2<-((subset(tmledat,tr==contrast[2])))
     nomiss2<-sort(unique(activeOnly2$pair))
     miss2<-(unique(pair)[which(!( unique(pair)%in%(nomiss2) ))])
     miss<-append(miss1,miss2)
     tmledat<-subset(tmledat,!(pair %in% miss))
     n.sub  <- dim(tmledat)[1]
-    if(print==TRUE)if(n.orig>n.sub) cat("\n-----------------------------------------\n","Starting N:  ",n.orig,"\nN after block dropping: ",n.sub)
-    if(print==TRUE)if(n.orig>n.sub) cat("\n-----------------------------------------\n","Pairs/blocks dropped due to missingness in at least one treatment level:\n",sort(unique(miss)),"\n\nDropping",n.orig-n.sub,"observations due to missing pairs.","\n-----------------------------------------\n")
+    if( (print==TRUE) & (n.orig>n.sub) ) {
+      cat("\n-----------------------------------------","\nThere were",length(unique(miss)),"pairs dropped because they were\nmissing at least one treatment level.\nThis is the list of their IDs:\n",sort(unique(miss)))
+      cat("\n-----------------------------------------","\nStarting N:  ",n.orig,"\nN after dropping incomplete blocks: ",n.sub,"\n\nTotal of",n.orig-n.sub,"observations dropped\n because of unmatched pairs.","\n-----------------------------------------\n")
+    }
   }
 
 
@@ -124,53 +114,65 @@ washb_tmle <- function(Y,tr,W=NULL,id,pair=NULL, Delta = rep(1,length(Y)),family
   n_orig <- dim(tmledat)[1]
   tmledat <- tmledat[complete.cases(tmledat),]
   n_sub  <- dim(tmledat)[1]
-  if(print==TRUE){if(n_orig>n_sub) cat("\n-----------------------------------------\nDropping",n_orig-n_sub,"observations due to missing\nvalues in one or more variables\n"," Final sample size:",n_sub,"\n-----------------------------------------\n")}
+  if( (print==TRUE) & (n_orig>n_sub)) {
+    cat("\n-----------------------------------------\nTotal of",n_orig-n_sub,"observations dropped due to missing\nvalues in one or more variables\n"," Final sample size:",n_sub,"\n-----------------------------------------\n")
+    }
 
-  # if specified, create a vector of treatment probabilities, depending on the type of contrast
-  # for control vs. intervention, the control prob should be 2/3 and tr prob should be 1/3 (allocation 2:1)
-  # for intervention arm 1 vs intervantion arm 2, prob should be 1/2 for both (allocation 1:1)
-  if(is.null(prtr)) {
-    tr_p <- NULL
-  } else {
-    tr_p <- ifelse(tmledat$tr==contrast[1],prtr[1],prtr[2])
-    g.SL.library <- NULL
-  }
 
   # pre-screen the covariates (if specified)
   # see washb_prescreen() in the base functions
-  if(is.null(W)){
-    # if W is null, create two empty variables so that tmle() can run
-    # (does not affect estimation)
-    Wselect <- data.frame(w1=rep(1,length(tmledat$Y)),w2=rep(1,length(tmledat$Y)))
-    Q.SL.library <- c("SL.glm")
-    g.SL.library <- c("SL.glm")
-  } else{
+  if(!is.null(W)){
 
-        if(print==TRUE){cat("\n-----------------------------------------\nPre-screening the adjustment covariates:\n-----------------------------------------\n")}
-    Wscreen <- washb_prescreen(Y=tmledat$Y,Ws=tmledat[,5:ncol(tmledat)],family=family,pval=pval,print=print)
+    if(print==TRUE){
+      cat("\n-----------------------------------------\nPre-screening the adjustment covariates\nusing a univariate liklihood ratio test:\n-----------------------------------------\n")
+      }
 
+    Wscreen <- washb_prescreen(Y=tmledat$Y,Ws=tmledat[,names(W)],family=family,pval=pval,print=print)
 
-    if(print==TRUE){cat("\n-----------------------------------------\n")}
+    if(print==TRUE){
+      cat("\n-----------------------------------------\n")
+      }
 
+    # format selected covariates (if any) for TMLE
     # for covariates that are factors, create indicator variables
     # because the tmle() function cannot handle factors in W
     # this relies on an internal function called design_matrix included in the washb package
     if(length(Wscreen)>0){
-      Wselect <- subset(tmledat[,5:ncol(tmledat)],select=Wscreen)
+      Wselect <- tmledat[,Wscreen]
       Wselect <- design_matrix(Wselect)
-    } else{
-      if(print==TRUE){cat("\n\nNo covariates were associated with the outcome\nProceeding with no adjustment...")}
-      # create a data frame with 2 columns of 1s -- allows tmle() to run, but does no adjustment
-      Wselect <- data.frame(w1=rep(1,length(tmledat$Y)),w2=rep(1,length(tmledat$Y)))
-    }
 
+      # if 1 or fewer covariates are selected, but glmnet is included in the library,
+      # remove it from the library because glmnet (lasso) requires 2 or more covariates to run
+      if(ncol(Wselect)<=1 & (length(grep("SL.glmnet",Q.SL.library)) + length(grep("SL.glmnet",g.SL.library)) >0)){
+        cat("\nNOTE: Dropping SL.glmnet from the library\nbecause there is only 1 covariate selected and glmnet\nrequires 2+ covariates to run\n")
+        Q.SL.library <- Q.SL.library[-grep("SL.glmnet",Q.SL.library)]
+        g.SL.library <- g.SL.library[-grep("SL.glmnet",g.SL.library)]
+      }
+
+    } else{
+        cat("\n\nSince no covariates were associated with the outcome,\nthe estimates below are unadjusted...")
+        if(n_orig>n_sub){
+          cat("\n\nIn this case, since",n_orig-n_sub,"observations were dropped\ndue to missing covariates,\nthose observations were not included in the analysis.\nIt would be best to re-estimate the effect without covariates (W=NULL)\nto avoid unnecessarily dropping these observations.")
+        }
+        W <- NULL
+    }
   }
 
-  # re-parse the variables to use them in tmle()
+  # if W is null or if no covariates were selected
+  # create a data frame with 2 columns of 1s -- allows tmle() to run, but does no adjustment
+  # and set the SL libraries to GLM only
+  if(is.null(W)) {
+    Wselect <- data.frame(w1=rep(1,length(tmledat$Y)),w2=rep(1,length(tmledat$Y)))
+    Q.SL.library <- c("SL.glm")
+    g.SL.library <- c("SL.glm")
+  }
+
+
+  # re-parse the variables and final-format to use them in tmle()
   tmle_Y <- tmledat$Y
   tmle_A <- ifelse(tmledat$tr==contrast[2],1,0)
   tmle_Delta <- tmledat$Delta
-  tmle_id <- tmledat$id
+  tmle_id <- as.numeric(tmledat$id)
 
   # estimate the ITT using TMLE
   if(!is.null(seed)) set.seed(seed)
@@ -178,16 +180,15 @@ washb_tmle <- function(Y,tr,W=NULL,id,pair=NULL, Delta = rep(1,length(Y)),family
                    A=tmle_A,
                    W=Wselect,
                    Delta=tmle_Delta,
-                   id=as.numeric(tmle_id),
+                   id=tmle_id,
                    family=family,
                    Q.SL.library=Q.SL.library,
-                   g1W=tr_p,
                    g.SL.library=g.SL.library
   )
   if(print==TRUE){
-  cat("\n-----------------------------------------\nEstimation Results:\n-----------------------------------------\n")
-  print(summary(tmle_fit))
-  cat("\n-----------------------------------------\n")
+    cat("\n-----------------------------------------\nEstimation Results:\n-----------------------------------------\n")
+    print(summary(tmle_fit))
+    cat("\n-----------------------------------------\n")
   }
   return(tmle_fit)
 }
